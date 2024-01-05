5f37ad25c3d2680b26c572e581ff9cd512feb6c5
nigolapalanona <152204169+NigolaPaLaNona@users.noreply.github.com>
2024-01-05 10:39:43 -0300

    deleted unused imports

--- books/src/test/java/com/books/books/controller/BookControllerTest.java
+++ books/src/test/java/com/books/books/controller/BookControllerTest.java
@@ -12,10 +12,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.ResponseEntity;
 import java.util.Arrays;
-import java.util.List;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 @SpringBootTest
 class BookControllerTest {
 
