079e96195c2229ba4983d0ebeac72becbfdc017b
nigolapalanona <152204169+NigolaPaLaNona@users.noreply.github.com>
2024-01-05 10:36:00 -0300

    change in BookControllerTest, add annotation @SprinBootTest

--- books/src/test/java/com/books/books/controller/BookControllerTest.java
+++ books/src/test/java/com/books/books/controller/BookControllerTest.java
@@ -8,23 +8,16 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.test.web.servlet.MockMvc;
-
 import java.util.Arrays;
 import java.util.List;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
-@WebMvcTest(BookController.class)
+@SpringBootTest
 class BookControllerTest {
-
-    @Autowired
-    MockMvc mockMvc;
 
     @MockBean
     private BookService bookService;
